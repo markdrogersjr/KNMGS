9c2141c400233b1394dad756be8b2d3aca9ddb81
Mark Rogers <34704945+OmniZeal@users.noreply.github.com>
2020-06-09 22:48:10 -0400

    Updated to V0.3.0
    * Updated license to use to GPL-3.0.
    * Updated Content Notice.
    * Provided an exit command for troubleshooting. While it terminates the bot instance properly, its still crashing upon
        the exit() call. Still working on that...
    * Modified randomlist generation so that anytime it selects League of Legends, it appends a random LoL game mode to the
        string.
    * Added more content to the statusList.
    * Removed parody and dark humored content. Users can decide that.
    * Changed the order of patch notes so that the most recent is at the top.
    * Updated the name of the file to match the public instance of this code on GitHub to reduce my confusion.

--- knmgs-public.py
+++ knmgs-public.py
@@ -3,39 +3,67 @@ Kira 'n Mark's Game Selector
 Idea by Kira Than
 Code by Mark Rogers
 https://github.com/OmniZeal/KNMGS
-Version: 0.2.2
+Version: 0.3.0
 Written with Python 3.6
 Utilizes Discord.py 1.3.0a
 
-LICENSE TO USE: There is no license. I'm not a big company, I don't care if you use my code. Just don't be evil and
-    claim it as your own work when it's not, 'cause that's not very nice. Free to use, open source, etc etc. Feel free
-    to make your own changes, as long as you declare which parts are yours and which parts are mine. I make no claims
-    to the proper operation of this code nor hold liability in any faults it may cause with any software and/or
-    hardware in which it interacts with. Use at your own risk.
+LICENSE TO USE: GPL-3.0
+    Copyright (C) 2019-2020 <Mark Rogers>
 
-DESCRIPTION: Code executes a Discord Bot utilizing the Discord.py API system, allowing the bot to communicate with
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+DESCRIPTION:
+    Code executes a Discord Bot utilizing the Discord.py API system, allowing the bot to communicate with
     discord users in order to select items from a random list. In practice, this was used for selecting a random game
     to play from a list, but could be used to select any random item from the list which can be interacted with via
     Discord.
 
+BOT TOKEN NOTE:
+    For personal instances of this bot, you are required to have a Bot Token, which is provided by Discord. Please see
+    their developer console on discord.com for more information.
+
 PATCH NOTES:
-V0.2.1 -
-* Added a Daily Game Option that sets the bot to automatically iterate a new game every 24 hours.
-* Added a Meme-y Status Update to Change the game the bot is playing every 30 minutes
-* Added delete_after statements to all bot messages. Future plans to implement for all messages between bot and users.
-* Added basic logging in the python shell to track the bot as it works through code, for easy debugging.
-* Added feature to add a new game to the gameList
+V0.3.0 -
+* Updated license to use to GPL-3.0.
+* Updated Content Notice.
+* Provided an exit command for troubleshooting. While it terminates the bot instance properly, its still crashing upon
+    the exit() call. Still working on that...
+* Modified randomlist generation so that anytime it selects League of Legends, it appends a random LoL game mode to the
+    string.
+* Added more content to the statusList.
+* Removed parody and dark humored content. Users can decide that.
+* Changed the order of patch notes so that the most recent is at the top.
+* Updated the name of the file to match the public instance of this code on GitHub to reduce my confusion.
 
 V0.2.2 -
 * Added a print list feature to show the current items in the list
 * Added a new game to list feature to allow users to add new games that can be randomly rolled
 * Added a remove game from list feature to allow users to remove games from the game list.
 
+V0.2.1 -
+* Added a Daily Game Option that sets the bot to automatically iterate a new game every 24 hours. This has to be
+    manually started. Working on options to make this automatic at Bot deployment to a server.
+* Added a Meme-y Status Update to Change the game the bot is playing every 30 minutes
+* Added delete_after statements to all bot messages. Future plans to implement for all messages between bot and users.
+* Added basic logging in the python shell to track the bot as it works through code, for easy debugging. A wise man
+    once said: "Print is my debugger."
+* Added feature to add a new game to the gameList
+
 Patch & Feature Road-map:
-* Daily Game active flag to track when the counter is on and off to prevent multiple instances
-* Modify message system to produce embedded messages for beautification
-* Add a game questionnaire that runs a logic test on the user to produce a random game with user specified variables
-    in mind.
+* Daily Game active flag to track when the counter is on and off to prevent multiple instances.
+* Modify message system to produce embedded messages for beautification.
+* Add a game questionnaire that runs a logic test on the user to produce a random game.
 * delete_after statements for user messages specifically to bot.
 
 """
@@ -57,64 +85,51 @@ BOT_PREFIX = "=="
 
 bot = commands.Bot(command_prefix=BOT_PREFIX)
 
-# Popped out per line to help with emojis later. Lines commented to modify list. Recommend keeping this formatted for
-# ease of exclusions with a simple comment delimiter.
+# Recommend keeping this formatted for ease of exclusions with a simple comment delimiter.
 # This list can be modified prior to start/execution of the bot, tailoring it to it's specific use.
-# What is already present in the list is for my own personal use.
-gameList = ['The Division 2',
-            'League of Legends',
-            'GTA Online',
-            'Risk of Rain 2',
-            'Rainbow Six Siege',
-            'Civilization V',
-            # "Player Unknown's Battlegrounds",
-            # 'SIMS 3',
-            'Rocket League',
-            'Minecraft',
-            'Final Fantasy XV',
-            'Ring of Elysium',
-            "Don't Starve Together",
-            # 'World of Warcraft',
-            # 'Battlerite',
-            'Destiny 2',
-            # 'Black Ops 4',
-            # 'Fortnite',
-            # 'Escape from Tarkov',
-            'Apex Legends']
+gameList = []
 
-statusList = [
-    # Random lists of 'Bot is Playing...' status can be added here for your benefit.
-]
+# Used to give a custom 'Playing...' status in Discord. Modify with your own content as you see fit.
+statusList = []
 
-leagueGames = ['Teamfight Tactics', 'Special Gamemode OR ARAM', 'Normal Draft', 'Twisted Treeline']
+# If you play League of Legends at all.
+leagueGames = ['Teamfight Tactics', 'Normal Draft', 'URF or ARAM']
 
 # Don't really know why I needed this, but it required tracemalloc so here it is *shrug*
 tracemalloc.start(25)
 
+# Terminal license prompt.
+print("Kira 'n Mark's Game Selector' Copyright (C) 2019-2020  Mark Rogers" + '\n'
+    "This program comes with ABSOLUTELY NO WARRANTY." + '\n'
+    "This is free software, and you are welcome to redistribute it under certain conditions." + '\n'
+    "Please see http://www.gnu.org/licenses/ for more information." + '\n')
+
+print('Starting Bot...' + '\n')
 
 # Start up provides console notification that bot has started, begins timer iteration of new Statuses
 # ======================================================================================================================
 @bot.event
 async def on_ready():
+
     count = 0
 
     bot.is_ready()
     activity = discord.Game(name=random.choice(statusList))
     await bot.change_presence(activity=activity, status=discord.Status('online'))
-    print(str(datetime.datetime.now()))
+
     print('-------------------BOT LOGIN-------------------')
     print('Bot is logged in as: ')
     print('Username: ' + bot.user.name)
     print('User ID: ' + str(bot.user.id))
+    print('Time: ' + str(datetime.datetime.now()))
     print('-------------------BOT READY-------------------')
     print('BOT LOGS:')
 
     print(str(datetime.datetime.now()) + ': Status Timer initializing...')
 
-    # Four hours in seconds: 14400
-    # Status timer function executes to display a new status from statusList above every four hours.
+    # 14400
     while count < 14400:
-        await asyncio.sleep(0.9999)
+        await asyncio.sleep(0.999)
         count = count + 1
 
         if count == 14400:
@@ -153,7 +168,10 @@ async def clear_list(ctx):
 @bot.command(name='ng++', description='Selects a random game from the list.', pass_context=True)
 async def game_selector(ctx):
     print(str(datetime.datetime.now()) + ': game_selector executed')
-    await ctx.send(content='Here you go: ' + random.choice(gameList), delete_after=180.0)
+    game = random.choice(gameList)
+    if game == str('League of Legends'):
+        game = str(game + ": " + random.choice(leagueGames))
+    await ctx.send(content='Here you go - ' + game, delete_after=180.0)
     print(str(datetime.datetime.now()) + ': Game selected!')
 
 
@@ -164,17 +182,24 @@ async def game_selector(ctx):
 async def daily_game(ctx):
     print(str(datetime.datetime.now()) + ': daily_game executed')
     count = 0
-    await ctx.send('The Game of the Day is: ' + random.choice(gameList), delete_after=86400.0)
+    game_otd = random.choice(gameList)
+    if game_otd == str('League of Legends'):
+        game_otd = str(game_otd + ": " + random.choice(leagueGames))
+
+    await ctx.send('The Game of the Day is ' + game_otd, delete_after=86400.0)
     print(str(datetime.datetime.now()) + ': New Daily Game sent! Starting new Daily Game Timer...')
 
     while count < 86400:
-        await asyncio.sleep(0.9999)
-        count = count + 1
+        await asyncio.sleep(1)
+        count = count + 0.9
 
-        if count == 86400:
+        if count >= 86400:
             print(str(datetime.datetime.now()) + ': Daily Game Timer complete...')
             count = 0
-            await ctx.send(content='The Game of the Day is: ' + random.choice(gameList), delete_after=86400.0)
+            game_otd = random.choice(gameList)
+            if game_otd == str('League of Legends'):
+                game_otd = str(game_otd + ": " + random.choice(leagueGames))
+            await ctx.send('The Game of the Day is ' + game_otd, delete_after=86400.0)
             print(str(datetime.datetime.now()) + ': New Daily Game sent! Starting new Daily Game Timer...')
 
 
@@ -183,7 +208,14 @@ async def daily_game(ctx):
 @bot.command(name='show', description='Prints the contents of the current Game List', pass_context=True)
 async def show_list(ctx):
     print(str(datetime.datetime.now()) + ': show_list executed')
-    await ctx.send(str(gameList))
+    item = 0
+    messageBody = ""
+    for item in range(0, len(gameList)):
+        if item == 0:
+            messageBody = '\u2022' + '\t' + '\t' + gameList[item] + '\n'
+        else:
+            messageBody = messageBody + '\u2022' + '\t' + '\t' + gameList[item] + '\n'
+    await ctx.send(str(messageBody))
     print(str(datetime.datetime.now()) + ': gameList has been displayed!')
 
 
@@ -213,6 +245,19 @@ async def delete_from_list(ctx):
 @bot.command(name='league', description='Prints out a selection of which league sub-game to play', pass_context=True)
 async def league_sub_game(ctx):
     await ctx.send(random.choice(leagueGames), delete_after=60.0)
+
+
+# Bot terminates itself.
+# ======================================================================================================================
+@bot.command(name='order66', description='Program Terminates', pass_context=True)
+async def terminate(ctx):
+    print(str(datetime.datetime.now()) + ': Shutdown executed!')
+    await ctx.send("Bravo Six, going Dark")
+    await bot.logout()
+    print(str(datetime.datetime.now()) + ': Logout Completed...')
+    await bot.close()
+    print(str(datetime.datetime.now()) + ': Bot Closed. Exiting to console...')
+    await exit(0)
 
 
 # Execute bot
